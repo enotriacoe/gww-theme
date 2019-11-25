7c2a9d61959a2087766ed6b0324c69bb457f9c35
Connor Fox <connor@cyber-duck.co.uk>
2019-11-25 19:32:15 +0000

    Linter changes from today

--- great-western-wine-theme/assets/js/theme/category.js
+++ great-western-wine-theme/assets/js/theme/category.js
@@ -107,12 +107,12 @@ export default class Category extends CatalogPage {
             if (screen.width >= 801) {
                 maxLength = 440;
             }
-          
+
             $('.show-read-more').each(function () {
                 const myStr = $(this).text();
                 if ($.trim(myStr).length > maxLength) {
                     let newStr = myStr.substring(0, maxLength);
-                    newStr = newStr.substr(0, Math.min(newStr.length, newStr.lastIndexOf(" ")))
+                    newStr = newStr.substr(0, Math.min(newStr.length, newStr.lastIndexOf(' ')));
                     const removedStr = myStr.substring(newStr.length, $.trim(myStr).length);
                     $(this).empty().html(newStr);
                     $(this).append('<span class="read-more-dots">...</span>');
