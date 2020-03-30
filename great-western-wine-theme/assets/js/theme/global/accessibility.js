4854148ba17cf1d9de2fe248f4f6e7586608a465
Connor Fox <connor@cyber-duck.co.uk>
2020-03-30 13:33:47 +0100

    Additional ESLint fix

--- great-western-wine-theme/assets/js/theme/global/accessibility.js
+++ great-western-wine-theme/assets/js/theme/global/accessibility.js
@@ -1,6 +1,6 @@
 const googleMapWrapper = document.getElementById('google-map');
 let googleMapIframe;
-if (googleMapWrapper != null) {
+if (googleMapWrapper !== null) {
     googleMapIframe = googleMapWrapper.getElementsByTagName('iframe')[0];
     googleMapIframe.setAttribute('title', 'Our Location');
 }
