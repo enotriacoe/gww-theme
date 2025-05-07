afbd78ddccd68a67d7ea56de15d1c39239ba40a1
Richard Weaver <r.weaver@enotriacoe.com>
2025-05-07 11:17:23 +0100

    fix: opaque modal background on add to basket

--- great-western-wine-theme/assets/js/theme/global/foundation.js
+++ great-western-wine-theme/assets/js/theme/global/foundation.js
@@ -6,21 +6,21 @@ import modalFactory from './modal';
 import revealCloseFactory from './reveal-close';
 
 export default function ($element) {
-    $element.foundation({
-        dropdown: {
-            // specify the class used for active dropdowns
-            active_class: 'is-open',
-        },
-        reveal: {
-            bg_class: 'modal-background',
-            dismiss_modal_class: 'modal-close',
-            close_on_background_click: true,
-        },
-        tab: {
-            active_class: 'is-active',
-        },
-    });
+  $element.foundation({
+    dropdown: {
+      // specify the class used for active dropdowns
+      active_class: 'is-open',
+    },
+    reveal: {
+      bg_class: 'modal-background',
+      dismiss_modal_class: 'modal-close',
+      close_on_background_click: true,
+    },
+    tab: {
+      active_class: 'is-active',
+    },
+  });
 
-    modalFactory('[data-reveal]', { $context: $element });
-    revealCloseFactory('[data-reveal-close]', { $context: $element });
+  modalFactory('[data-reveal]', { $context: $element });
+  revealCloseFactory('[data-reveal-close]', { $context: $element });
 }
